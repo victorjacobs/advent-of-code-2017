ab83d035de4fd1c2f50c4b5a80b6b7858fe60200
Victor Jacobs <victor.jacobs@me.com>
2017-12-14 21:51:05 +0100

    Day 4

---
+++ 4.highEntropyPassphrases/part1.hs
@@ -0,0 +1,14 @@
+import Data.Char
+import Data.List.Extra
+import Data.Maybe
+
+main :: IO ()
+main = do
+    content <- readFile "input.txt"
+    print $ length $ filter not $ map (hasDuplicates [] . splitOn " ") (lines $ trim content)
+
+hasDuplicates :: [String] -> [String] -> Bool
+hasDuplicates _ [] = False
+hasDuplicates ws (x:xs)
+  | elem x ws = True
+  | otherwise = hasDuplicates (x:ws) xs
